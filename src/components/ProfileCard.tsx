ede9626b49362e3b16407db6c4476766b8788a24
Rohid Mondal <rohidmondal.dev@gmail.com>
2025-12-10 11:38:55 +0530

    profile card ui fix

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -5,7 +5,7 @@ const ProfileCard = () => {
 	return (
 		<>
 			<Card className="py-0">
-				<CardHeader className="h-32 w-full rounded-xl bg-[url(/cover.jpg)] bg-cover bg-center bg-no-repeat"></CardHeader>
+				<CardHeader className="h-32 w-full rounded-t-xl bg-[url(/cover.jpg)] bg-cover bg-center bg-no-repeat"></CardHeader>
 
 				<CardContent>
 					<div className="grid grid-cols-3 gap-5">
@@ -20,11 +20,9 @@ const ProfileCard = () => {
 					</div>
 				</CardContent>
 
-				<CardFooter>
-					<div className="grid w-full grid-cols-2 gap-3 pb-1">
-						<Button className="bg-red-600">Delete</Button>
-						<Button className="bg-green-600">Edit</Button>
-					</div>
+				<CardFooter className="grid w-full grid-cols-2 gap-3 pb-4">
+					<Button className="bg-red-600">Delete</Button>
+					<Button className="bg-green-600">Edit</Button>
 				</CardFooter>
 			</Card>
 		</>
