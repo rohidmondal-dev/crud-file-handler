4718aee5185db4e2e18c1e26ee998bff36b8d8bf
Rohid Mondal <rohidmondal.dev@gmail.com>
2025-12-24 11:56:35 +0530

    prisma setup & dependencies bump

---
+++ prisma/migrations/20251224060628/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "contact" INTEGER NOT NULL,
+    "profileImage" TEXT NOT NULL,
+    "coverImage" TEXT NOT NULL
+);
